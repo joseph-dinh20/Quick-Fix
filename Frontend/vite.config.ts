ff4993fe543f22d33e1fd39c1517d36617010b97
devhaocui <devhaocui@gmail.com>
2026-02-23 20:00:40 -0800

    attempt to update pnpm-lock.yaml

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
+// vite.config.ts
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), vueDevTools()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
