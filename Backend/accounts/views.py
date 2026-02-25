dece4aa4ce15c4b150eed8f250480a8d18df5cb9
Mark <diaz.mark112@gmail.com>
2026-02-24 17:11:01 -0800

    Add session login & logout endpoints, cleaned tracked cache and db

--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -4,6 +4,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Profile
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, login as dj_login, logout as dj_logout
+from rest_framework import status
 
 @api_view(["POST"])
 def signup(request):
@@ -26,3 +29,32 @@ def signup(request):
     profile.save()
 
     return Response({"message": "User created"})
+
+user = get_user_model()
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if not email or not password:
+        return Response({"error": "email and password are required"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.filter(email=email).first()
+    if not user:
+        return Response({"error": "user does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(request, username=user.username, password=password)
+    if not user:
+        return Response({"error": "invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+    dj_login(request, user)
+
+    return Response(
+        {"message": "logged in", "user": {"id": user.id, "username": user.username, "email": user.email}},
+        status=status.HTTP_200_OK
+    )
+
+@api_view(["POST"])
+def logout(request):
+    dj_logout(request)
+    return Response({"message": "logged out"}, status=status.HTTP_200_OK)
