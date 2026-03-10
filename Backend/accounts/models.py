303b6b5cc6c43fdbbb02c83590eadd7a4a2637dc
ioseph01 <24601karma@gmail.com>
2026-03-09 20:35:41 -0700

    simple services database

--- Backend/accounts/models.py
+++ Backend/accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from services.models import Service
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
@@ -24,3 +25,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name or self.user.username
+    
+
+class ServiceProvider(models.Model):
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+
+    services = models.ManyToManyField(Service, related_name="providers")
+
+    price_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
+    about_me = models.TextField(blank=True)
+
+    total_rating = models.IntegerField(default=0)
+    average_rating = models.DecimalField(max_digits=3, decimal_places=2, default=0)
--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'api',
     'accounts',
+    'services',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
