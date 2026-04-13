1c45800ed9bc40e9b692fa007edbc4108eebe7e9
ioseph01 <j0ni0n.clang@gmail.com>
2026-04-12 22:17:25 -0700

    Money sign and location before I sleep

--- Frontend/src/components/DemoMyJobs.vue
+++ Frontend/src/components/DemoMyJobs.vue
@@ -32,7 +32,7 @@ import {
   ChevronDown, 
   DollarSign, 
   Info,
-  ImageIcon 
+  ImageIcon,
 } from "lucide-vue-next"
 
 import {
@@ -45,6 +45,7 @@ import {
 // ---------------------------------------------------
 
 import ServiceSelect from "./ServiceSelect.vue"
+import CardTitle from "./ui/card/CardTitle.vue"
 
 const jobs = ref([])
 const currentSort = ref("Sort by")
@@ -291,7 +292,15 @@ onMounted(fetchJobs)
         >
           <CardContent class="p-5 flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
             <div>
-              <h3 class="font-bold text-[#1a202c] text-[15px] mb-2.5">{{ job.title }}</h3>
+              <h2 class="font-bold text-[#1a202c] text-[15px] mb-2.5">{{ job.title }}</h2>
+              <h2 class="font-bold text-[#1a202c] text-[15px] mb-2 flex items-center">
+                <DollarSign :size="16" />{{ job.budget }}</h2>
+              <div class="flex items-center gap-3 text-slate-500 font-medium pb-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=""><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path><circle cx="12" cy="10" r="3"></circle></svg>
+                <span>
+                {{ job.city + ', ' + job.state || 'No Location Provided' }}
+                </span>
+              </div>
               <div class="flex items-center gap-3 text-sm">
                 <Badge 
                   variant="outline" 
