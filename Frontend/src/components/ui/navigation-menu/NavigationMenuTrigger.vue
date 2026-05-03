6c9a9e9dcc82625b1f83b46602870712c97ded0b
ioseph01 <j0ni0n.clang@gmail.com>
2026-05-02 20:25:45 -0700

    hover indicator

--- Frontend/src/components/ui/navigation-menu/NavigationMenuTrigger.vue
+++ Frontend/src/components/ui/navigation-menu/NavigationMenuTrigger.vue
@@ -17,7 +17,7 @@ const forwardedProps = useForwardProps(delegatedProps)
     v-bind="forwardedProps"
     :class="
       cn(
-        'group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-mauve3 hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50',
+        'group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 focus:bg-gray-100  focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=open]:bg-gray-100',
         props.class,
       )
     "
